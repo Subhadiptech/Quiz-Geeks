18d5ee3ce28c000d403d8fcef175761fb60f190b
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-30 23:37:54 +0530

    updated

---
+++ app/src/main/java/com/ersubhadip/quizgeeks/Student/StudentsAuthActivity.java
@@ -0,0 +1,16 @@
+package com.ersubhadip.quizgeeks.Student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ersubhadip.quizgeeks.R;
+
+public class StudentsAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_auth);
+    }
+}
---
+++ app/src/main/java/com/ersubhadip/quizgeeks/Teacher/TeacherAuthActivity.java
@@ -0,0 +1,16 @@
+package com.ersubhadip.quizgeeks.Teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ersubhadip.quizgeeks.R;
+
+public class TeacherAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_auth);
+    }
+}
